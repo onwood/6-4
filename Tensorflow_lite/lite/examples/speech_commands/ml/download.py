b05fd9fee8f04301114f735313e07f5fa7f8fac6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 16:22:55 +0000

    Adding numeric_owner as keyword arguement

--- Tensorflow_lite/lite/examples/speech_commands/ml/download.py
+++ Tensorflow_lite/lite/examples/speech_commands/ml/download.py
@@ -50,7 +50,7 @@ with tarfile.open(ARCHIVE, 'r:gz') as tar:
           if not is_within_directory(path, member_path):
               raise Exception("Attempted Path Traversal in Tar File")
   
-      tar.extractall(path, members, numeric_owner) 
+      tar.extractall(path, members, numeric_owner=numeric_owner) 
       
   
   safe_extract(tar, path="data/train")
